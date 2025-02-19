b1f0d0a3b406a97aee2cec2255201099fd4df6b3
methus364 <Agustinmathus3@gmail.com>
2025-02-19 22:44:02 +0700

    add Hero component

--- src/component/user/Hero.jsx
+++ src/component/user/Hero.jsx
@@ -1,10 +1,26 @@
 
+const heroImage = "https://cdn.pixabay.com/photo/2024/06/21/12/45/lake-8844310_960_720.jpg"
+
 const Hero = () => {
   return (
     <>
-        <div className="container-fuid">
-            
+      <div
+        className="container-fluid bg-dark d-flex flex-column  align-items-center justify-content-center"
+        style={{
+          height: "100vh",
+          backgroundImage: `url(${heroImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}>
+        <div>
+          <div className="bg-white d-flex flex-column  align-items-center justify-content-center " style={{ height: "65vh", width: "55vh" }}>
+            <div>
+              <h1>LOGIN</h1>
+            </div>
+          </div>
         </div>
+      </div>
     </>
   )
 }
